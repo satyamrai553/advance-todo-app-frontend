7f8e6097becf3cbbab0f8bd2b4dc81a9a8f4da55
Satyam Rai <satyamrai553@gamil.com>
2024-07-12 15:43:22 +0530

    input and update on fontend screen completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import axios from "axios"
 import Nav from "./assets/Nav"
 import { useEffect, useState } from "react"
 import Card from "./assets/Card";
+import { CirclePlus } from 'lucide-react'; 
+import InputBox from "./components/InputBox";
 
 
 
 function App() {
  const [data, setData] = useState([]);
+ const[showModal, setShowModal] = useState(false);
  useEffect(()=>{
   axios.get('/api/v1/todo/read')
   .then((response) => {
@@ -24,11 +27,17 @@ function App() {
   return (
     <><div className="w-screen h-screen bg-[#0B0C10]">
      <Nav/>
-      <h1 className="text-6xl font-bold font-mono text-[#CADCFC] text-center mt-6">Todo App</h1>
+     <div className="flex justify-center relative">
+     <h1 className="text-6xl font-bold font-mono text-[#CADCFC] text-center mt-6">Todo App</h1>
+     <button onClick={()=>setShowModal(true)} className="absolute top-12 right-52"><CirclePlus size={36} color="#ffffff" /></button>
+     </div>
+      
     
         <div className="flex justify-center mt-10">{data.map((data)=>{
           return <Card key={data._id} title={data.title} description={data.description}/>
         })}</div>
+
+      {showModal && <InputBox onClose={() => setShowModal(false)}/>}
       
     </div>
     
---
+++ src/components/InputBox.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {X} from 'lucide-react'
+import axios from "axios";
+
+import { useState } from "react"
+
+function InputBox({onClose}) {
+
+
+    const [title,setTitle] = useState("");
+    const [description,setDescription] = useState("");
+
+
+    
+const addtodoHandler = async ()=>{
+    
+    try {
+      await axios.post('/api/v1/todo/add', {title, description});
+      setTitle("")
+      setDescription("")
+      onClose();
+    
+    } catch (error) {
+      console.error("Error occurred:", error);
+    }
+    
+  };
+
+
+  return (
+    <div className='fixed inset-0 bg-yellow-300 bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
+        <div className='mt-10 flex flex-col gap-5'>
+        <button onClick={onClose} className='place-self-end'><X size={30}/></button>
+        
+        <div className='bg-yellow-100 rounded-xl py-10 flex flex-col gap-5 items-center w-72'>
+        <h1 className='text-xl font-sans font-black text-amber-700  '>Add Todo</h1>
+        <input  className="border-lime-300 border-2 text-lg text-center" value={title} type="text" onChange={(e) => setTitle(e.target.value)} placeholder="Enter Title" />
+        <textarea className="w-52 text-lg h-20" value={description} type="text" onChange={(e) => setDescription(e.target.value)} placeholder="Enter Description" ></textarea>
+       
+        <button className='bg-blue-500 w-24 rounded-xl p-2 text-white font-sans font-medium' onClick={addtodoHandler}>Add</button>
+        </div>
+        </div>
+        </div>
+  )
+}
+
+export default InputBox
